c527f5c12cd7beb2b970b02c199f2ab0a9c8e7ee
Marya Doery <mdoery@gmail.com>
2018-11-04 16:25:50 -0500

    Add a readme with the current view of the application. Change the app so that it has 3 simple panels.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Awesome Hangboard Interval Timer #
+
+Here's what it looks like after messing around with the UI today. I did a small demo [like-clock-ui-demo](https://github.com/mdoery/like-clock-ui-demo) to get the layout set up.
+
+![three horizontal panels](https://raw.githubusercontent.com/mdoery/awesome-hangboard-interval-timer/master/2018-11-04-awesome-hangboard-interval-timer.png)
--- app/src/main/java/com/mdoery/hangboardintervaltimer/MainActivity.java
+++ app/src/main/java/com/mdoery/hangboardintervaltimer/MainActivity.java
@@ -5,41 +5,50 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
+    private TextView txtFeedback;
+    private TextView editText;
+/*
+    A user will input the interval time.
+    At first I thought TextView input but now I'm thinking something very similar to the timer that
+    comes w my device. This appears to have an Apache License, so I'll check to see if it can be
+    incorporated.
+    I like the idea of using a roll-y thing to flip through HH:MM:SS
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        txtFeedback = (TextView) findViewById(R.id.txtFeedback);
+
+        /*
+        Button btnAddInterval = (Button) findViewById(R.id.btnAddInterval);
+        btnAddInterval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                log("Add Interval Button Clicked! " + text);
+            }
+        });
+        */
+        Button btnStart = (Button) findViewById(R.id.startToggleButton);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                log("Start Button Clicked!!");
+            }
+        });
+    }
+
+    private void log(String msg) {
+        System.out.println(msg);
+        txtFeedback.setText(msg);
     }
 
 }
